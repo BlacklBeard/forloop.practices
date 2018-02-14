b8d5e1a8a8262358b5f7c818d9ab76360e68df31
BlacklBeard <raiyan-hk@hotmail.com>
2018-02-15 07:54:45 +0800

    Write a program in C to display n terms of natural number and their sum

--- finding the sum of all natural.c
+++ finding the sum of all natural.c
@@ -3,7 +3,8 @@
 int main(){
 	
 	int arr[10] = {1, 2 , 3, 4, 5, 6, 7, 8, 9, 10};
-	int i, sum;
+	int i;
+	int sum = 0;
 	
 	printf("the first 10 natural numbers are ");
 	
@@ -11,7 +12,9 @@ int main(){
 	for (i = 0; i <= 9; i++){
 		printf("\n%d ", arr[i]);
 		
+		sum = sum + arr[i];
+			
 	}
-	
+	printf("\n\nthe sum is %d ", sum);
 	return 0;
 }
